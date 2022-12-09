1ec32eeda644a83b9be478b3dfb7513c245f5a35
ParhamInBinart <parham.berenjian@gmail.com>
2022-12-09 11:27:09 +0100

    Created new file for 'Add event window' for cleaner code

---
+++ public/JavaScript/openAddEventWindow.js
@@ -0,0 +1,7 @@
+/**
+ * Opens a popup window upon click.
+ */
+export function openAddEventWindow() {
+    const addEventWindow = document.querySelector(".addEventWindow");
+    addEventWindow.classList.toggle("displayFlex")
+}
