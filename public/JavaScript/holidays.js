5be5b2a7227ce2586aeeed4d4128650115e34c7b
lauax <lucas12alfredsson@gmail.com>
2022-12-15 10:39:02 +0100

    början på holidays

---
+++ public/JavaScript/holidays.js
@@ -0,0 +1,32 @@
+var JAN = 1,
+    FEB = 2,
+    MAR = 3,
+    APR = 4,
+    MAJ = 5,
+    JUN = 6,
+    JUL = 7,
+    AUG = 8,
+    SEP = 9,
+    OCT = 10,
+    NOV = 11,
+    DEC = 12;
+
+    var 
+        Days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+       
+
+const holidays = {
+    Nyårsdagen:[Days, JAN],
+    Trettondedag:[Days, JAN],
+    Långfredagen: [Days, JAN],
+    Påskdagen: [Days, JAN],
+    AnnandagPåsk: [Days, JAN],
+    FörstaMaj: [Days, JAN],
+    KristiHimmelfärdsdag: [Days, JAN],
+    Pingstdagen: [Days, JAN],
+    Sverigesnationaldag: [Days, JAN],
+    Midsommardagen: [Days, JAN],
+    AllaHelgonsDag: [Days, JAN],
+    Juldagen: [Days, JAN],
+    Annandagjul: [Days, JAN],
+}
