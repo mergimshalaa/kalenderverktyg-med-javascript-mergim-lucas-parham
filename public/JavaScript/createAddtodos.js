0980a9aacebc13d5c35af9d5387bfa78c95fce4c
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2022-12-15 20:32:19 +0100

    add addeventlistener on addtodoitem

--- public/JavaScript/createAddtodos.js
+++ public/JavaScript/createAddtodos.js
@@ -1,9 +1,10 @@
 function addItemToDo() {
+    document.querySelector('#addToDo').addEventListener('click', () => {
+
     const task = document.getElementById("taskInput").value;
     const li = document.createElement('li');
+
     li.innerText = task;
     document.getElementById("tasks").appendChild(li);
+    })
 }
-
-
-//Add function to remove todo item
