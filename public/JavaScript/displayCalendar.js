72a5f072a61c5cfc44417287fd3e0d4be1286a28
ParhamInBinart <parham.berenjian@gmail.com>
2022-12-15 11:21:09 +0100

    prettier, classname edit

--- public/JavaScript/displayCalendar.js
+++ public/JavaScript/displayCalendar.js
@@ -1,62 +1,71 @@
 let currentMonth = 0;
 
 function displayCalendar() {
-    const calendarDisplay = document.querySelector(".calendarDisplay");
-    const monthDisplay = document.querySelector(".monthDisplay");
-    const weekArray = ["måndag", "tisdag", "Onsdag", "torsdag", "fredag", "lördag", "söndag"];
+  const calendarDisplay = document.querySelector(".calendarDisplay");
+  const monthDisplay = document.querySelector(".monthDisplay");
+  const weekArray = [
+    "måndag",
+    "tisdag",
+    "Onsdag",
+    "torsdag",
+    "fredag",
+    "lördag",
+    "söndag",
+  ];
 
-    const date = new Date();
+  const date = new Date();
 
-    if ( currentMonth !== 0 ) {
-        date.setMonth( new Date().getMonth() + currentMonth );
+  if (currentMonth !== 0) {
+    date.setMonth(new Date().getMonth() + currentMonth);
+  }
+
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  const firstDayOfMonth = new Date(year, month, 1);
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+  monthDisplay.textContent = `${date.toLocaleString("sv-SE", {
+    month: "long",
+    year: "numeric",
+  })}`;
+
+  const dateString = firstDayOfMonth.toLocaleDateString("sv-SE", {
+    weekday: "long",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+
+  const emptyCalendarSpace = weekArray.indexOf(dateString.split(", ")[0]);
+
+  calendarDisplay.innerHTML = "";
+
+  for (let i = 1; i <= emptyCalendarSpace + daysInMonth; i++) {
+    const calendarDay = document.createElement("div");
+    calendarDay.classList.add("calendarDay");
+
+    if (i > emptyCalendarSpace) {
+      calendarDay.innerText = i - emptyCalendarSpace;
+
+      calendarDay.addEventListener("click", () => {});
+    } else {
+      calendarDay.classList.add("emptyCalendarSpace");
     }
 
-    const day = date.getDate();
-    const month = date.getMonth();
-    const year = date.getFullYear();
-
-    const firstDayOfMonth = new Date(year, month, 1);
-    const daysInMonth = new Date( year, month + 1, 0).getDate();
-
-    monthDisplay.textContent = `${date.toLocaleString("sv-SE", { month: "long", year: "numeric"})}`;
-
-    const dateString = firstDayOfMonth.toLocaleDateString("sv-SE", {
-        weekday: "long",
-        year: "numeric",
-        month: "numeric",
-        day: "numeric",
-    });
-
-    const emptyCalendarSpace = weekArray.indexOf(dateString.split(", ")[0]);
-
-    calendarDisplay.innerHTML = "";
-
-    for ( let i = 1; i <= emptyCalendarSpace + daysInMonth; i++) {
-        const calendarDay = document.createElement("div");
-        calendarDay.classList.add("calendarDay");
-
-        if ( i > emptyCalendarSpace ) {
-            calendarDay.innerText = i - emptyCalendarSpace;
-
-            calendarDay.addEventListener("click", () => {
-
-            })
-        } else {
-            calendarDay.classList.add("emptyCalendarSpace");
-        }
-
-        calendarDisplay.appendChild(calendarDay);
-    }
+    calendarDisplay.appendChild(calendarDay);
+  }
 }
 
 function changeMonthView() {
-    
-    document.querySelector(".fa-angle-left").addEventListener("click", () => {
-        currentMonth--;
-        displayCalendar();
-    });
-    document.querySelector(".fa-angle-right").addEventListener("click", () => {
-        currentMonth++;
-        displayCalendar();
-    });
+  document.querySelector(".prevMonth").addEventListener("click", () => {
+    currentMonth--;
+    displayCalendar();
+    console.log(currentMonth)
+  });
+  document.querySelector(".nextMonth").addEventListener("click", () => {
+    currentMonth++;
+    displayCalendar();
+  });
 }
