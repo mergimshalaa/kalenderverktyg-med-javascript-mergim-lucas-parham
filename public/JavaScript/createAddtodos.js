5ef63e4cbfccd06dcdb6e02eba7011ad7f4dc423
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2022-12-16 17:19:16 +0100

    rechanged funtion and added to delete todos

--- public/JavaScript/createAddtodos.js
+++ public/JavaScript/createAddtodos.js
@@ -1,10 +1,25 @@
 function addItemToDo() {
-    document.querySelector('#addToDo').addEventListener('click', () => {
-
+  document.querySelector("#addToDo").addEventListener("click", () => {
     const task = document.getElementById("taskInput").value;
-    const li = document.createElement('li');
+    const li = document.createElement("li");
+
+    const deleteIcon = document.createElement("i");
+    deleteIcon.classList.add("fa-solid", "fa-trash-can", "trash");
+
+    const div = document.createElement("div");
+    div.classList.add("container-icon");
+
+    document.getElementById("tasks").append(li);
+
+    deleteIcon.addEventListener("click", () => {
+      removeItemToDo(li);
+    });
 
     li.innerText = task;
-    document.getElementById("tasks").appendChild(li);
-    })
+    li.append(deleteIcon);
+  });
+}
+
+function removeItemToDo(item) {
+  item.remove();
 }
