20a5d2421e2c8b241fc68633e076bb177a99fd6f
lauax <lucas12alfredsson@gmail.com>
2022-12-17 12:02:15 +0100

    Update startTimeAndDate.js

--- public/JavaScript/startTimeAndDate.js
+++ public/JavaScript/startTimeAndDate.js
@@ -11,7 +11,45 @@ function startClock() {
     renderWeekday();
 }
 
-function renderWeekday() {
+function renderWeekday() {window.addEventListener('DOMContentLoaded', main);
+
+async function main() {
+  const dates = await fetchLastFiveMidsummerDays();
+  renderMidsummerDates(dates);
+}
+
+function renderMidsummerDates(dates) {
+  const listElements = dates.map(date => {
+    const li = document.createElement('li');
+    li.textContent = date;
+    return li;
+  })
+  const ul = document.querySelector('ul');
+  ul.append(...listElements);
+ }
+
+async function fetchLastFiveMidsummerDays() {
+  const currentYear = new Date().getFullYear();
+
+  const midsummerDates = [];
+  for (let year = currentYear; year > currentYear - 5; year--) {
+    const midsummerDate = await fetchMidsummerDay(year);
+    midsummerDates.push(midsummerDate);
+  }
+  return midsummerDates;
+}
+
+async function fetchMidsummerDay(year) {
+  const url = `https://sholiday.faboul.se/dagar/v2.1/${year}/01-12`;
+  const response = await fetch(url);
+  const result = await response.json();
+
+  for (const day of result.dagar) {
+    if (day.helgdag === "Midsommarafton") {
+      return day.datum
+    }
+  }
+}
     const weekday = new Date();
     const weekdayToString = weekday.toLocaleString("sv-SE", {
         weekday: "long",
