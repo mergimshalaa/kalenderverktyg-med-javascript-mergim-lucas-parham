f6d98fa012e45c5d9b789f861e7880e4f2ea1213
ParhamInBinart <parham.berenjian@gmail.com>
2022-12-19 22:12:22 +0100

    reworking function for adding a task

--- public/JavaScript/createAddtodos.js
+++ public/JavaScript/createAddtodos.js
@@ -1,59 +1,115 @@
 function addItemToDo() {
-  document.querySelector("#addToDo").addEventListener("click", () => {
-  const task = document.getElementById("taskInput").value;
+  const storedTasks = JSON.parse(localStorage.getItem("tasks"));
 
-  const dateTodo = document.getElementById("dateTodo").value;
+  if (!storedTasks) {
+    return;
+  }
+  const taskList = document.getElementById("taskList");
+  taskList.textContent = "";
 
-   const taskList = document.createElement('div');
-   taskList.classList.add('task');
+  for ( const task of storedTasks ) {
 
-   const taskContent = document.createElement('div')
-   taskContent.classList.add('content');
-   taskList.append(taskContent);
+    const taskInput = document.getElementById("taskInput");
+    const dateInput = document.getElementById("dateTodo");
 
-   const taskInput = document.createElement('input');
-   taskInput.classList.add('textInput');
-   taskInput.type = 'text';
-   taskInput.value = task;
-   taskInput.setAttribute('readonly', 'readonly');
+    const taskItem = document.createElement("div");
+    taskItem.classList.add("task");
+    
+    const taskTextAndDateContainer = document.createElement("div");
+    taskTextAndDateContainer.classList.add("content");
 
-   const taskActions = document.createElement('div');
-   taskActions.classList.add('taskActions');
-   
-   const taskEdit = document.createElement('button');
-   taskEdit.classList.add('edit')
-   taskEdit.innerText = 'Edit';
+    const taskText = document.createElement("div");
+    taskText.classList.add("taskText");
+    // taskText.setAttribute("readonly", "readonly");
+
+    const dateContainer = document.createElement("div");
+    dateContainer.classList.add("dateContainer");
+
+    const taskActions = document.createElement("div");
+    taskActions.classList.add("taskActions");
+
+    const taskEdit = document.createElement("button");
+    taskEdit.classList.add("edit");
 
     const deleteTask = document.createElement("i");
     deleteTask.classList.add("fa-solid", "fa-trash-can", "trash");
+    
+    taskActions.append(taskEdit, deleteTask);
+    taskTextAndDateContainer.append(taskText, dateContainer);
+    taskItem.append(taskTextAndDateContainer, taskActions);
+    taskList.append(taskItem);
 
-    taskActions.append(taskEdit);
-    taskActions.append(deleteTask);
-    taskContent.append(taskInput, dateTodo);
-    taskList.append(taskActions);
+    taskText.textContent = task.taskTitle;
+    dateContainer.textContent = task.date;
+    taskEdit.textContent = "Edit";
 
-    document.getElementById('tasks').append(taskList);
+    // taskEdit.addEventListener("click", () => {
+    //   const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+    //   taskInput.removeAttribute("readonly");
+    //   taskEdit.textContent = "Save";
+    //   
+    //   
+    // });
 
-    taskEdit.addEventListener('click', () => {
-      if (taskEdit.innerText.toLowerCase() == "edit") {
-        taskEdit.innerText = "Save";
-        taskInput.removeAttribute("readonly");
-        taskInput.focus()
-      } else {
-        taskEdit.innerText = "Edit";
-        taskInput.setAttribute('readonly', 'readonly');
-      }
-    })
 
-    deleteTask.addEventListener('click', () => {
-      document.getElementById("tasks").removeChild(taskList);
+    // const task = document.getElementById("taskInput").value;
+
+    // const dateTodo = document.getElementById("dateTodo").value;
+
+    // const taskList = document.createElement("div");
+    // taskList.classList.add("task");
+
+    // const taskContent = document.createElement("div");
+    // taskContent.classList.add("content");
+    // taskList.append(taskContent);
+
+    // const taskInput = document.createElement("input");
+    // taskInput.classList.add("textInput");
+    // taskInput.type = "text";
+    // taskInput.value = task;
+    // taskInput.setAttribute("readonly", "readonly");
+
+    // const taskActions = document.createElement("div");
+    // taskActions.classList.add("taskActions");
+
+    // const taskEdit = document.createElement("button");
+    // taskEdit.classList.add("edit");
+    // taskEdit.innerText = "Edit";
+
+    // const deleteTask = document.createElement("i");
+    // deleteTask.classList.add("fa-solid", "fa-trash-can", "trash");
+
+    // taskActions.append(taskEdit);
+    // taskActions.append(deleteTask);
+    // taskContent.append(taskInput, dateTodo);
+    // taskList.append(taskActions);
+
+    // document.getElementById("taskList").append(taskList);
+
+    // taskEdit.addEventListener("click", () => {
+    //   if (taskEdit.innerText.toLowerCase() == "edit") {
+    //     taskEdit.innerText = "Save";
+    //     taskInput.removeAttribute("readonly");
+    //     taskInput.focus();
+    //   } else {
+    //     taskEdit.innerText = "Edit";
+    //     taskInput.setAttribute("readonly", "readonly");
+    //   }
+    // });
+
+    deleteTask.addEventListener("click", () => {
+      const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+      const task = storedTasks.find((task) => task.id === task.id );
+      storedTasks.splice(task, 1);
+      localStorage.setItem("tasks", JSON.stringify(storedTasks));
+      taskItem.remove();
       updateTodoCount();
-    })
+    });
     updateTodoCount();
-  })
+  }
 }
 
 function updateTodoCount() {
-  const todoCount = document.querySelectorAll('.task').length;
-  document.querySelector('#todoCount').textContent = `${todoCount}`;
+  const todoCount = document.querySelectorAll(".task").length;
+  document.querySelector("#todoCount").textContent = `${todoCount}`;
 }
